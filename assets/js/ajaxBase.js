dbc8f3e0830af78ddecfc66084b620a97975add6
wk <742751416@qq.com>
2021-01-06 12:15:11 +0800

    个人中心基本资料功能完成

--- assets/js/ajaxBase.js
+++ assets/js/ajaxBase.js
@@ -1,13 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2021-01-05 10:33:03
- * @LastEditTime: 2021-01-05 22:14:22
+ * @LastEditTime: 2021-01-06 09:34:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \bigevent\assets\js\ajaxBase.js
  */
 $.ajaxPrefilter(function(options){
-    // console.log(options);
+    console.log(options);
     options.url= 'http://api-breakingnews-web.itheima.net'+options.url
 
 // 判断 需要身份认证信息的时候 ，地址携带‘/my’的时候
@@ -16,6 +16,11 @@ $.ajaxPrefilter(function(options){
             Authorization:localStorage.getItem('token')
         }
     }
-    
+    // 限制用户访问权限
+    options.complete=function(xhr){
+        if (xhr.responseJSON.status===1&&xhr.responseJSON.message==='身份认证失败！') {
+            location.href='/home/login.html' 
+        }
+    }
 
 })
